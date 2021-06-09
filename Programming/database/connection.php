591cff5f8d01ebd445475e3040fb29d5ec4bd1af
SAYEED AJMAL <SAYEEDAJMALA06@GMAIL.COM>
2021-06-09 02:30:05 +0000

    added function of string to lowercase

--- Programming/database/connection.php
+++ Programming/database/connection.php
@@ -1,31 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body class="text-center">
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "PERSONAL";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $name = $_POST['username'];
-    $pass = $_POST['password'];
-    echo '$name';
-    echo '$pass';
-    echo "Connected successfully";
-    ?>
-</body>
-
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "PERSONAL";
+$database = "Programming";
+// Create a connection 
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection Error" . mysqli_connect_error());
+}
+?>
--- Programming/index.php
+++ Programming/index.php
@@ -10,25 +10,25 @@
 </head>
 
 <body>
-  <form method="POST" action="Dashboard.php">
+  <form method="POST" action="retrieve.php">
     <h1 class="login">Login</h1>
     <div class="container">
       <img src="images/Test.jpg" class="avatar" />
       <br />
       <!-- USERNAME -->
       <br />
-      <label class="label">Username</label>
-      <input class="input" type="text" placeholder="Enter Your Name" name="username" />
+      <label class="label">Email ID</label>
+      <input class="input" type="email" placeholder="Enter Your Email" name="email" required />
       <br />
       <br />
       <!-- PASSWORD -->
       <label class="label">Password</label>
-      <input class="input" type="password" placeholder="Enter Your Password" name="password" />
+      <input class="input" type="password" placeholder="Enter Your Password" name="password" required />
       <!-- Submit Button -->
       <input class="submit" type="submit" name="submit" value="Login" />
       <a href="#" class="forgot">Forgot password?</a>
       <br />
-      <a class="signup" href="signup.html" style="margin-top: 10px;">Signup</a>
+      <a class="signup" href="signup.php" style="margin-top: 10px;">Signup</a>
     </div>
   </form>
 </body>
